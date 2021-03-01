67c7ae1699e8b7ab9da869faf299d336092a22a1
Willow Systems <mail@willow.systems>
2021-03-01 16:45:37 +0000

    Update search to show message on no results

--- res/js/search.js
+++ res/js/search.js
@@ -4,17 +4,25 @@ function performSearch() {
     console.log(searchTerm.length)
     if (searchTerm.length < 1) {
         $('.searchitem').removeClass("hidden");
+        $('#topicfilter').html("All topics:");
+        $("#nothingFound").addClass("hidden");
         return
+    } else {
+        $('#topicfilter').html("Relevant topics:");
     }
 
     var searchWords = searchTerm.split(" ");
-
+    var foundSolutions = 0;
+    
     $(".searchitem").each(function (index) {
         var keywords = $(this).attr("data-keywords");
         var hide = true
         for (var i=0; i<searchWords.length;i++) {
             console.log("Does " + keywords + " include " + searchWords[i])
-            if (keywords.includes(searchWords[i])) { hide = false }
+            if (searchWords[i].length > 1 && keywords.includes(searchWords[i])) { 
+                hide = false;
+                foundSolutions++
+            }
         }
         if (hide) {
             $(this).addClass("hidden");
@@ -22,6 +30,13 @@ function performSearch() {
             $(this).removeClass("hidden");
         }
     });
+
+    if (foundSolutions == 0) {
+        $("#nothingFound").removeClass("hidden");
+    } else {
+        $("#nothingFound").addClass("hidden");
+    }
+
 }
 
 $('#searchbar').on('keyup',function(e){
